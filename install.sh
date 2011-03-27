73a74d8e94172e5aa6506801c45907edc7fad3c3
Hubert Behaghel <hub@cosmac.home>
2011-03-27 21:18:55 +0200

    Added installation of oh-my-zsh

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+# this is a very basic script:
+#   no uninstall
+#   no partial install (eg without oh-my-zsh)
+
 
 for i in `cat .tobedotlinked`; do
-  ln -s $i ~/.$i
+  ln -s $PWD/$i ~/.$i
 done
+
+# oh-my-zsh
+#wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 
 
