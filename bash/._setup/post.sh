007001682a4db2693e30fb92d487dc2dc0c54a58
behaghel <behaghel@gmail.com>
2020-11-08 17:36:43 +0000

    [bash] reload after install

--- bash/._setup/post.sh
+++ bash/._setup/post.sh
@@ -3,3 +3,4 @@
 # we don't provide our own ~/.profile as systems tend to have their
 # own …and you never know
 echo "for i in $HOME/.config/profile.d/*.profile; do source $i; done" >> ~/.profile
+exec bash
