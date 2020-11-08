e58b62cfe9aa3a14356ce830cbc27560a51a5fb1
behaghel <behaghel@gmail.com>
2020-11-08 17:46:21 +0000

    [bash] getting there

--- bash/._setup/post.sh
+++ bash/._setup/post.sh
@@ -2,5 +2,5 @@
 
 # we don't provide our own ~/.profile as systems tend to have their
 # own …and you never know
-echo "for i in $HOME/.config/profile.d/*.profile; do source $i; done" >> ~/.profile
+echo 'for i in $HOME/.config/profile.d/*.profile; do source $i; done' >> ~/.profile
 exec bash
