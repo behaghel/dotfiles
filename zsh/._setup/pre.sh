29c8ff27df60847378ba2d0fb41d557a783ba218
behaghel <behaghel@gmail.com>
2020-11-14 15:52:42 +0000

    [zsh] fix: dont launch zsh before the ability is stowed

--- zsh/._setup/pre.sh
+++ zsh/._setup/pre.sh
@@ -2,4 +2,4 @@
 
 # set up ZSH
 # APT update upgrade install list from install.apt goes here
-export KEEP_ZSH=yes && export RUN_ZSH=no && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+export KEEP_ZSH=yes && export RUNZSH=no && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
