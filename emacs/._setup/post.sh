46a10855f6cebd7dcd9367f96b20366159af88aa
behaghel <behaghel@gmail.com>
2020-12-03 22:51:06 +0000

    [emacs] fix install
    + remove tinkering with apt sources

--- emacs/._setup/post.sh
+++ emacs/._setup/post.sh
@@ -8,7 +8,7 @@ if (( $EUID != 0 )); then
     sudo='sudo'
 fi
 # ensure at least one deb-src in /etc/apt/sources.list
-$sudo sed -i '/deb-src/s/^# //' /etc/apt/sources.list && $sudo apt update
+# $sudo sed -i '/deb-src/s/^# //' /etc/apt/sources.list && $sudo apt update
 $sudo apt-get build-dep emacs
 ./autogen.sh
 ./configure CFLAGS='-O3'
@@ -16,7 +16,7 @@ make
 $sudo make install
 make clean
 
-cd $HOME/.local/share/fonts
+local_fonts_dir=$HOME/.local/share/fonts
 iosevka_version=3.6.1
 hasklig_version=1.1
 jetbrainsmono_version=2.001
@@ -25,6 +25,8 @@ https://github.com/be5invis/Iosevka/releases/download/v$iosevka_version/ttf-iose
 https://github.com/i-tu/Hasklig/releases/download/$hasklig_version/Hasklig-$hasklig_version.zip
 https://download.jetbrains.com/fonts/JetBrainsMono-$jetbrainsmono_version.zip
 )
+mkdir -p $local_fonts_dir 2> /dev/null
+cd $local_fonts_dir
 # install apps available to local users and root
 for font in ${fonts[@]}; do
   wget $font
