bf669cbb39f1b28e3a1b75e9097b176ff37030bb
Hubert Behaghel <behaghel@gmail.com>
2021-07-10 17:49:22 +0100

    [weechat] adds matrix client

--- nix/.config/nixpkgs/home.nix
+++ nix/.config/nixpkgs/home.nix
@@ -1,11 +1,18 @@
 { config, pkgs, ... }:
 
 let
+  # weechat-matrix broken in stable: can't find matrix_helper_sso
+  # still broken in unstable but less so: it now wants me to identify
+  # myself only through facebook/google/...
+  weechat-matrix = pkgs.python3Packages.callPackage <nixos-unstable/pkgs/applications/networking/irc/weechat/scripts/weechat-matrix> {};
   weechat = pkgs.weechat.override { configure = { availablePlugins, ... }: {
     plugins = with availablePlugins; [
-        (perl.withPackages (p: [ p.PodParser ])) # PodParser for Pod::Select.pm for menu.pl
+        # PodParser for Pod::Select.pm for menu.pl for spell suggestions
+        (perl.withPackages (p: [ p.PodParser ]))
+        # websocket_client for wee-slack and dbus-python for notifications
         (python.withPackages (ps: with ps; [ websocket_client dbus-python ]))
       ];
+    scripts = [ weechat-matrix ];
     };
   };
 in {
