ae8817e515de063d0d92173eea0a1e1c93781edf
Hubert Behaghel <behaghel@gmail.com>
2021-10-24 21:57:07 +0100

    oops

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  }
+  };
 
   outputs = { self, nixpkgs, homeManager, nur }: {
     homeConfigurations = {
