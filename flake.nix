38ba00dcc85b2c1e442ee21d8faf8414ddacc6df
Hubert Behaghel <behaghel@gmail.com>
2021-10-24 21:53:22 +0100

    Using homeManagerConfiguration function

--- flake.nix
+++ flake.nix
@@ -2,15 +2,26 @@
   description = "hub's home";
 
   # Nix User Repository: user-contributed packages
-  inputs.nur.url = "github:nix-community/NUR";
+  inputs = {
+    nur.url = "github:nix-community/NUR";
+    homeManager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  }
 
-  outputs = { self, nixpkgs, nur }: {
-    nixosModules = {
-      dotfiles = {
-        imports = [
-          (./nix/.config/nixpkgs/home.nix)
-          { nixpkgs.overlays = [ nur.overlay ]; }
-        ];
+  outputs = { self, nixpkgs, homeManager, nur }: {
+    homeConfigurations = {
+      "hub@dell-laptop" = homeManager.lib.homeManagerConfiguration {
+        configuration = import ./nix/.config/nixpkgs/home.nix;
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          overlays = [ nur.overlay ];
+        };
+        system = "x86_64-linux";
+        homeDirectory = "/home/hub";
+        username = "hub";
+        stateVersion = "21.05";
       };
     };
   };
--- nix/.config/nixpkgs/home.nix
+++ nix/.config/nixpkgs/home.nix
@@ -21,11 +21,6 @@ in {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "hub";
-  home.homeDirectory = "/home/hub";
-
   # TODO: DRY
   # technically that should look like
   # home.file.".config".source = ../../../bash/.config;
@@ -114,14 +109,4 @@ in {
 
   # https://github.com/nix-community/lorri
   services.lorri.enable = true;
-
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "21.03";
 }
