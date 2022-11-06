82d161bd5e772e05721bc886ea8616d41ac284b9
Hubert Behaghel <behaghel@gmail.com>
2022-11-06 10:26:31 +0100

    [latex] pdflatex for my resume needs metafont

--- flake.nix
+++ flake.nix
@@ -465,7 +465,7 @@ SyncState "*"
                 };
                 texlive = {
                   enable = true;
-                  extraPackages =  tpkgs: { inherit (tpkgs) scheme-basic wrapfig amsmath ulem hyperref capt-of xcolor dvisvgm dvipng; };
+                  extraPackages =  tpkgs: { inherit (tpkgs) scheme-basic wrapfig amsmath ulem hyperref capt-of xcolor dvisvgm dvipng metafont; };
                 };
 
               };
