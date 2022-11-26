5de54f8c838031f09f6c6a78717bcb3b3a3f01e1
Hubert Behaghel <behaghel@gmail.com>
2022-11-26 22:51:05 +0100

    [darwin] migrating to latest mk-darwin-system
    Necessary with latest stable nix version (defaultApp deprecated)

--- flake.nix
+++ flake.nix
@@ -5,13 +5,13 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nur.url = "github:nix-community/NUR";
-    nur.inputs.nixpkgs.follows = "nixpkgs";
+    # nur.inputs.nixpkgs.follows = "nixpkgs";
     # emacs-overlay.url = "github:nix-community/emacs-overlay";
     home-manager = {
       url = "github:nix-community/home-manager/release-22.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    mk-darwin-system.url = "github:vic/mk-darwin-system/v0.2.0";
+    mk-darwin-system.url = "github:vic/mk-darwin-system/1321309223d7ef11937bd2539e2da77e5b7e0151";
     mk-darwin-system.inputs.nixpkgs.follows = "nixpkgs";
   };
 
@@ -20,147 +20,69 @@
       # TODO: bring this home config and ./macos/._setup/users/hub.nix
       # together: what should be common / specific etc.
       home = import ./nix/.config/nixpkgs/home.nix;
-      darwinFlakeOutput = mk-darwin-system.mkDarwinSystem.m1 {
-
-        # Provide your nix modules to enable configurations on your system.
-        #
-        modules = [
-          # You might want to split them into separate files
-          #  ./modules/host-one-module.nix
-          #  ./modules/user-one-module.nix
-          #  ./modules/user-two-module.nix
-          # Or you can inline them here, eg.
-
-          # for configurable nix-darwin modules see
-          # https://github.com/LnL7/nix-darwin/blob/master/modules/module-list.nix
-          ({ config, pkgs, ... }: {
-            environment.systemPackages = with pkgs; [ niv hello ];
-          })
-
-          ./macos/._setup/darwin/system.nix
-
-          ({ pkgs, lib, ... }: {
-            # Fonts
-            fonts = {
-              fonts = with pkgs; [
-                # fonts that many things (apps, websites) expect
-                dejavu_fonts
-                liberation_ttf
-                roboto
-                raleway
-                ubuntu_font_family
-                # unfree
-                # corefonts
-                # helvetica-neue-lt-std
-                # fonts I use
-                etBook
-                emacs-all-the-icons-fonts
-                # coding fonts
-                source-sans-pro
-                source-serif-pro
-                (nerdfonts.override {
-                 enableWindowsFonts = true;
-                 fonts = [ "Iosevka" "FiraCode" "Hack" "Inconsolata" "JetBrainsMono" "Hasklig" "Meslo" "Noto" ];
-                })
-                font-awesome
-              ];
-              # the modern way in NixOS: fontDir.enable = true;
-              enableFontDir = true;
-            };
-
-            # System / General
-            # Recreate /run/current-system symlink after boot
-            services.activate-system.enable = true;
-          })
-
-
-          ./macos/._setup/darwin/yabai.nix
-          ./macos/._setup/services/sketchybar
-          ./macos/._setup/darwin/sketchybar
-          ./macos/._setup/darwin/skhd
-
-          # or configurable home-manager modules see:
-          # https://github.com/nix-community/home-manager/blob/master/modules/modules.nix
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-            };
-          }
-
-          ./macos/._setup/users/hub.nix
-
-          # for configurable nixos modules see (note that many of them might be linux-only):
-          # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/module-list.nix
-          ({ lib, ... }: {
-            nixpkgs.overlays = import ./macos/._setup/overlay.nix {
-              pkgsX86 = nixpkgs.legacyPackages.x86_64-darwin;
-              inherit (lib.mds) installNivDmg;
-              inherit nur;
-            };
-          })
-
-        ];
+      typeformFlake = mk-darwin-system.mkFlake rec {
+        userName = "hub";
+        hostName = "ES-658-GR3BFQ05PM";
+        flake = ./.;
+        hostModules = ["${flake}/macos/._setup/hosts/${hostName}.nix"];
+        userModules = ["${flake}/macos/._setup/users/${userName}.nix"];
       };
-    in darwinFlakeOutput // {
-      # Your custom flake output here.
-      darwinConfigurations."ES-658-GR3BFQ05PM" =
-        darwinFlakeOutput.darwinConfiguration.aarch64-darwin;
+    in typeformFlake;
+      #  // {
+      # homeConfigurations = {
+      #   "hub@dell-laptop" = home-manager.lib.homeManagerConfiguration {
+      #     configuration = home;
+      #     pkgs = import nixpkgs {
+      #       system = "x86_64-linux";
+      #       overlays = [ nur.overlay ];
+      #     };
+      #     system = "x86_64-linux";
+      #     homeDirectory = "/home/hub";
+      #     username = "hub";
+      #     userConfig = {
+      #       git.enable = true;
+      #       desktop.enable = true;
+      #       nix.lorri = true;
+      #     };
+      #   };
+      # };
+      # nixosConfigurations =
+      # let
+      #   system = "x86_64-linux";
+      #   pkgs = import nixpkgs {
+      #     inherit system;
+      #     config = import ./nix/.config/nixpkgs/config.nix;
+      #     overlays = [ nur.overlay ];
+      #   };
 
-      homeConfigurations = {
-        "hub@dell-laptop" = home-manager.lib.homeManagerConfiguration {
-          configuration = home;
-          pkgs = import nixpkgs {
-            system = "x86_64-linux";
-            overlays = [ nur.overlay ];
-          };
-          system = "x86_64-linux";
-          homeDirectory = "/home/hub";
-          username = "hub";
-          userConfig = {
-            git.enable = true;
-            desktop.enable = true;
-            nix.lorri = true;
-          };
-        };
-      };
-      nixosConfigurations =
-      let
-        system = "x86_64-linux";
-        pkgs = import nixpkgs {
-          inherit system;
-          config = import ./nix/.config/nixpkgs/config.nix;
-          overlays = [ nur.overlay ];
-        };
-
-        inherit (nixpkgs) lib;
-        util = import ./._setup/nix/lib {
-          inherit system pkgs home-manager lib;
-          overlays = (pkgs.overlays);
-        };
-        inherit (util) user;
-        inherit (util) host;
-      in {
-        dell-laptop = host.mkHost {
-          name = "dell-laptop";
-          kernelPackage = pkgs.linuxPackages;
-          # taken from hardware.nix
-          NICs = [];            # take it from hardware.nix
-          initrdMods = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
-          kernelMods = [ "kvm-intel" ];
-          kernelParams = [];
-          systemConfig = {
-            # you can shape here the config options that make sense to
-            # you through modules of your own
-          };
-          users = [{
-            name = "hub";
-            groups = [ "wheel" "networkmanager" "video" ];
-            uid = 1000;
-            shell = pkgs.zsh;
-          }];
-          cupCores = 4;
-        };
-      };
-    };
+      #   inherit (nixpkgs) lib;
+      #   util = import ./._setup/nix/lib {
+      #     inherit system pkgs home-manager lib;
+      #     overlays = (pkgs.overlays);
+      #   };
+      #   inherit (util) user;
+      #   inherit (util) host;
+      # in {
+      #   dell-laptop = host.mkHost {
+      #     name = "dell-laptop";
+      #     kernelPackage = pkgs.linuxPackages;
+      #     # taken from hardware.nix
+      #     NICs = [];            # take it from hardware.nix
+      #     initrdMods = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
+      #     kernelMods = [ "kvm-intel" ];
+      #     kernelParams = [];
+      #     systemConfig = {
+      #       # you can shape here the config options that make sense to
+      #       # you through modules of your own
+      #     };
+      #     users = [{
+      #       name = "hub";
+      #       groups = [ "wheel" "networkmanager" "video" ];
+      #       uid = 1000;
+      #       shell = pkgs.zsh;
+      #     }];
+      #     cpuCores = 4;
+      #   };
+    #   };
+    # };
 }
---
+++ macos/._setup/hosts/ES-658-GR3BFQ05PM.nix
@@ -0,0 +1,59 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+# for configurable nixos modules see (note that many of them might be linux-only):
+# https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/module-list.nix
+#
+# for configurable nix-darwin modules see
+# https://github.com/LnL7/nix-darwin/blob/master/modules/module-list.nix
+{
+  environment.systemPackages = with pkgs; [nixVersions.stable];
+
+  modules = [
+    ./../darwin/system.nix
+
+    ({ pkgs, lib, ... }: {
+      # Fonts
+      fonts = {
+        fonts = with pkgs; [
+          # fonts that many things (apps, websites) expect
+          dejavu_fonts
+          liberation_ttf
+          roboto
+          raleway
+          ubuntu_font_family
+          # unfree
+          # corefonts
+          # helvetica-neue-lt-std
+          # fonts I use
+          etBook
+          emacs-all-the-icons-fonts
+          # coding fonts
+          source-sans-pro
+          source-serif-pro
+          (nerdfonts.override {
+            enableWindowsFonts = true;
+            fonts = [ "Iosevka" "FiraCode" "Hack" "Inconsolata" "JetBrainsMono" "Hasklig" "Meslo" "Noto" ];
+          })
+          font-awesome
+        ];
+        # the modern way in NixOS: fontDir.enable = true;
+        enableFontDir = true;
+      };
+
+      # System / General
+      # Recreate /run/current-system symlink after boot
+      services.activate-system.enable = true;
+    })
+
+
+    ./../darwin/yabai.nix
+    ./../services/sketchybar
+    ./../darwin/sketchybar
+    ./../darwin/skhd
+
+  ];
+}
